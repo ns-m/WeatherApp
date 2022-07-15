bdd39d9209ec06f687ba9bf745b1aa937a23d0f0
KMV <9117479@gmail.com>
2022-07-15 12:53:28 +0300

    WeatherApp Design 3

---
+++ app/src/main/java/com/example/weatherapp/adapters/ViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.example.weatherapp.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fragAct: FragmentActivity): FragmentStateAdapter(fragAct) {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+}
