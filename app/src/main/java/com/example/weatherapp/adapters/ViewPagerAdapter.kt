4d2fbf4e84678b23dbbf3dcf3b1946026f5cfefb
KMV <9117479@gmail.com>
2022-07-22 17:42:57 +0300

    WeatherApp ViewPager 1

--- app/src/main/java/com/example/weatherapp/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/weatherapp/adapters/ViewPagerAdapter.kt
@@ -4,12 +4,14 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter(fragAct: FragmentActivity): FragmentStateAdapter(fragAct) {
+class ViewPagerAdapter(fragAct: FragmentActivity, private val list: List<Fragment>): FragmentStateAdapter(fragAct) {
+
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return list.size
     }
 
     override fun createFragment(position: Int): Fragment {
-        TODO("Not yet implemented")
+        return list[position]
     }
+
 }
--- app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp/fragments/MainFragment.kt
@@ -8,12 +8,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
 import com.example.weatherapp.R
+import com.example.weatherapp.adapters.ViewPagerAdapter
 import com.example.weatherapp.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainFragment : Fragment() {
     private lateinit var binding: FragmentMainBinding
     private lateinit var pLauncher: ActivityResultLauncher<String>
+    private val fragList = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance()
+    )
+    private val bttnList = listOf(
+        getString(R.string.temp_text_hours),
+        getString(R.string.temp_text_days)
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +37,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = ViewPagerAdapter(activity as FragmentActivity, fragList)
+        vwPgrDetails.adapter = adapter
+        TabLayoutMediator(tabLayout, vwPgrDetails){
+            tab, pos -> tab.text = bttnList[pos]
+        }.attach()
     }
 
     private fun permissionListener(){
