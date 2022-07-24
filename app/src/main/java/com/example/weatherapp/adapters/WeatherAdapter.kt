55e78eadaa99c3ecd2a7660308cf5dcb7d7c7b03
KMV <9117479@gmail.com>
2022-07-24 23:55:06 +0300

    WeatherApp Template 3

--- app/src/main/java/com/example/weatherapp/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/example/weatherapp/adapters/WeatherAdapter.kt
@@ -1,4 +1,18 @@
 package com.example.weatherapp.adapters
 
-class WeatherAdapter {
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp.databinding.ListItemBinding
+
+class WeatherAdapter: ListAdapter<WeatherModel, > {
+
+    class Holder(view: View): RecyclerView.ViewHolder(view){
+        val binding = ListItemBinding.bind(view)
+        fun bind(item: WeatherModel) = with(binding){
+            txVwDate.text = item.time
+            txVwConditionItem.text = item.condition
+            txVwTempC.text = item.currentTempC
+        }
+    }
 }
